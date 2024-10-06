d5dfd387b5070cfc94c06a23cd79250c4582c944
Mohamed Abassia <mo3bassia@gmail.com>
2024-10-06 19:59:41 +0300

    add react logic in accodrion component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,58 @@
 import "./styles.css";
+import { useState } from "react";
+
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
+];
 
 export default function App() {
-  return <h1>Accordion Questions</h1>;
+  return (
+    <div>
+      <Accordion />
+    </div>
+  );
+}
+
+function Accordion() {
+  return (
+    <div className="accordion">
+      <Item />
+    </div>
+  );
+}
+
+function Item() {
+  const [selected, setSelected] = useState();
+
+  return (
+    <>
+      {faqs.map((faq, index) => (
+        <div
+          className={`item ${index === selected ? "open" : ""}`}
+          key={index + 1}
+          onClick={() =>
+            index !== selected ? setSelected(index) : setSelected()
+          }
+        >
+          <div className="number">0{index + 1}</div>
+          <div className="title">{faq.title}</div>
+          <div className="icon">{index === selected ? "-" : "+"}</div>
+          {index === selected ? (
+            <div className="content-box">{faq.text}</div>
+          ) : null}
+        </div>
+      ))}
+    </>
+  );
 }
